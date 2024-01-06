13579980b26752b69b3076de61fb29ee1276a40a
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-06 18:05:36 +0900

    Title: 시험 통과 여부 확인하기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240106/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240106/시험 통과 여부 확인하기/verify-test-passed.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        if (a > 80) {
+        if (a >= 80) {
             System.out.println("pass");
         }
         else {
