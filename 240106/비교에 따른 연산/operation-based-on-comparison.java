ec22dfb144b4ffe793f2ad1af6b15de05e4a6ea2
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-06 18:10:03 +0900

    Title: 비교에 따른 연산, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240106/비교에 따른 연산/operation-based-on-comparison.java
+++ 240106/비교에 따른 연산/operation-based-on-comparison.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.println(a*b);
         }
         else {
-            System.out.println(a/b);
+            System.out.println(b/a);
         }
     }
 }
