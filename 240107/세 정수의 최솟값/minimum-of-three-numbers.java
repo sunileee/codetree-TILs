668bbc243cd6c7073181a1cc4eae80e3921ea168
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-07 20:08:15 +0900

    Title: 세 정수의 최솟값, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240107/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 240107/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -10,13 +10,15 @@ public class Main {
         if (a <= b && a <= c) {
             System.out.println(a);
         }
-        
-        if (b <= a && b <= c) {
+        else if (b <= a && b <= c) {
             System.out.println(b);
         }
 
-        if (c <= a && c <= b) {
+        else if (c <= a && c <= b) {
             System.out.println(c);
+        }
+        else {
+            System.out.print("");
         }
     }
 }
