1f05fd8d0d70817b3d93c512a37fd27af22bc556
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-08 21:31:36 +0900

    Title: 중앙값 구하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/중앙값 구하기/find-the-median.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a >= b && a >= c) {
+            if (b > c) {
+                System.out.print(b);
+            } else {
+                System.out.print(c);
+            }
+        } else if (b >= a && b >= c) {
+            if (a > c) {
+                System.out.print(a);
+            } else {
+                System.out.print(c);
+            }
+        } else {
+             if (a > b) {
+                System.out.print(a);
+            } else {
+                System.out.print(b);
+            }
+        }
+    }
+}
