4256c974c1b65a88a9a3fa5ad274faaf466bfab2
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-08 21:06:09 +0900

    Title: 윤년인가, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240108/윤년인가/is-leap-year.java
+++ 240108/윤년인가/is-leap-year.java
@@ -6,10 +6,10 @@ public class Main {
         int y = sc.nextInt();
 
         if (y % 4 == 0) {
-            if (y % 100 == 0) {
-                System.out.print("false");
-            } else if (y % 100 == 0 && y % 400 == 0) {
+            if (y % 100 == 0 && y % 400 == 0) {
                 System.out.print("true");
+            } else if (y % 100 == 0) {
+                System.out.print("false");
             } else {
                 System.out.print("true");
             }
