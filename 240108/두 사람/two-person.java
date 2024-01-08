1fc0053edd9d4376ee8a373e69f68dcb514a9ae2
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-08 20:49:34 +0900

    Title: 두 사람, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240108/두 사람/two-person.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aAge = sc.nextInt();
+        String aSex = sc.next();
+        int bAge = sc.nextInt();
+        String bSex = sc.next();
+
+        if ((aAge >= 19 || bAge >= 19) && (aSex.charAt(0) == 'M' || bSex.charAt(0) == 'M')) {
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
+    }
+}
