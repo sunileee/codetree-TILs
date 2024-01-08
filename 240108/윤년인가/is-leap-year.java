738632a17bbc5b8d02867e5c3d32060e04e0cc58
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-08 21:04:25 +0900

    Title: 윤년인가, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240108/윤년인가/is-leap-year.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        if (y % 4 == 0) {
+            if (y % 100 == 0) {
+                System.out.print("flase");
+            } else if (y % 100 == 0 && y % 400 == 0) {
+                System.out.print("true");
+            } else {
+                System.out.print("true");
+            }
+        } else {
+            System.out.print("false");
+        }
+    }
+}
