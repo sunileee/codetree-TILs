f6b3be6b66b930aa1a6bb0bbfeb93ec4215d1d19
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-09 15:05:07 +0900

    Title: 증가시키며 출력하기 2, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240109/증가시키며 출력하기 2/increase-and-print-2.java
+++ 240109/증가시키며 출력하기 2/increase-and-print-2.java
@@ -1,14 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        int i = 5;                  // 시작 값(초기화식)
+    public static void main(String[] args) {        
+        int i = 10;                  // 시작 값(초기화식)
 
-        while (i <= 10) {           // 계속 반복할 조건(조건식)
-            System.out.println(i);  // 원하는 코드 작성
+        while (i <= 26) {           // 계속 반복할 조건(조건식)
+            System.out.print(i + " ");  // 원하는 코드 작성
             i++;                    // 원하는 증감값 설정(증감식)
         }
     }
