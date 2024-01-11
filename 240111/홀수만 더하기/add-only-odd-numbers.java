0cd39169d31122d7d7db912d290366210474de3d
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-11 23:10:09 +0900

    Title: 홀수만 더하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sumVal = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int a = sc.nextInt();
+            
+            if (a % 2 != 0 && a % 3 == 0) {
+                sumVal += a;
+            }
+        }
+        
+        System.out.println(sumVal);
+    }
+}
