0de1fd41b345b23c134dba4109e2064a8fdc22d6
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-16 23:15:44 +0900

    Title: 조건에 따른 연산, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while(true) {
+            if (n % 2 == 0) {
+                n = (n * 3) + 1;
+            }
+            else {
+                n = (n * 2) + 2;
+            }
+
+            cnt++;
+
+
+            if (n >= 1000) 
+                break;
+        }
+
+        System.out.println(cnt);
+    }
+}
