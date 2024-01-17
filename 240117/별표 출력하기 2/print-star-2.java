6c30d2e1ce14b52bc3e4c8f5c0c72cc1450e79a0
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-17 14:13:00 +0900

    Title: 별표 출력하기 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240117/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = m; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            m--;
+            System.out.println();
+        }
+    }
+}
