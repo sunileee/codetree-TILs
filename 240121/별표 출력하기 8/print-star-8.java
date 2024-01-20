f7fe9508d3ff5e0299454051617a555393949bbe
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-21 02:47:06 +0900

    Title: 별표 출력하기 8, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240121/별표 출력하기 8/print-star-8.java
+++ 240121/별표 출력하기 8/print-star-8.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 0; i < n6; i++) {
+        for (int i = 0; i < n; i++) {
             if (i % 2 != 0) {
                 for (int j = 0; j < i + 1; j++) {
                     System.out.print("* ");
