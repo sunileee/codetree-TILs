c54a5d325ae495ba5c60b7c9ce8827a230a0fe6c
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-27 15:10:00 +0900

    Title: 일직선 구구단, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240127/일직선 구구단/one-line-multiple.java
+++ 240127/일직선 구구단/one-line-multiple.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < 3; j++) {
+            for (int j = 1; j <= n; j++) {
                 System.out.println(i + " * " + j + " = " + (i * j));
             }
         }
