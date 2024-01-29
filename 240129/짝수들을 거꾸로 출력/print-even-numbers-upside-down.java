290ff2682c3790e024a283414cf18ddd6c7b4717
sunileee <152301718+sunileee@users.noreply.github.com>
2024-01-29 13:47:40 +0900

    Title: 짝수들을 거꾸로 출력, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240129/짝수들을 거꾸로 출력/print-even-numbers-upside-down.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            if (m % 2 == 0) {
+                arr[i] = m;
+            }
+        }
+
+        for (int j = arr.length - 1; j >= 0; j--) {
+            if (arr[j] != 0) {
+                System.out.print(arr[j] + " ");
+            }
+        }
+    }
+}
