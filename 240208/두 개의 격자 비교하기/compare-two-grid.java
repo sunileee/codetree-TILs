d1908bfd8e0f1d4d098c762c07e3b96befbf5920
sunileee <152301718+sunileee@users.noreply.github.com>
2024-02-08 22:33:37 +0900

    Title: 두 개의 격자 비교하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/두 개의 격자 비교하기/compare-two-grid.java
+++ 240208/두 개의 격자 비교하기/compare-two-grid.java
@@ -10,26 +10,26 @@ public class Main {
         int arr2[][] = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 arr1[i][j] = sc.nextInt();
             }
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 arr2[i][j] = sc.nextInt();
             }
         }
 
         int arr3[][] = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 arr3[i][j] = arr1[i][j] == arr2[i][j] ? 0 : 1;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print(arr3[i][j] + " ");
             }
             System.out.println();
