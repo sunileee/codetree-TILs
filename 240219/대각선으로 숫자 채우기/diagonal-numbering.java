922fe43c7abace15989bbf6deb4eb259746df9d3
sunileee <152301718+sunileee@users.noreply.github.com>
2024-02-19 15:12:15 +0900

    Title: 대각선으로 숫자 채우기, Time: 248ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240219/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // n, m을 입력받습니다.
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int count = 1;
+
+        // 2차원 배열을 구현합니다.
+        int[][] arr = new int[100][100];
+
+        // Step 1:
+        for(int startCol = 0; startCol < m; startCol++) {
+            int currRow = 0;
+            int currCol = startCol;
+
+            while(currCol >= 0 && currRow < n) {
+                arr[currRow][currCol] = count;
+
+                currRow++;
+                currCol--;
+                count++;
+            }
+        }
+
+        for(int startRow = 1; startRow < n; startRow++) {
+            int currRow = startRow;
+            int currCol = m - 1;
+
+            while(currCol >= 0 && currRow < n) {
+                arr[currRow][currCol] = count;
+
+                currRow++;
+                currCol--;
+                count++;
+            }
+        }
+                
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < m; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
