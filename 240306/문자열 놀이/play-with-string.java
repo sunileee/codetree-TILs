f9fd6f1581f4968b516c877c3e5673ad0f06b77f
sunileee <152301718+sunileee@users.noreply.github.com>
2024-03-06 19:42:45 +0900

    Title: 문자열 놀이, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/문자열 놀이/play-with-string.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int q = sc.nextInt();
+
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < q; i++) {
+            int type = sc.nextInt();
+
+            if (type == 1) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                char temp = arr[a-1];
+                arr[a-1] = arr[b-1];
+                arr[b-1] = temp;
+                s = String.valueOf(arr);
+            }
+            else if (type == 2) {
+                char a = sc.next().charAt(0);
+                char b = sc.next().charAt(0);
+                
+                s = s.replace(a, b);
+            }
+
+            System.out.println(s);
+        }
+    }
+}
