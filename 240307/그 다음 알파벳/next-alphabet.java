1d3b05a09d226aafc4014e4d591ae513c084cf57
sunileee <152301718+sunileee@users.noreply.github.com>
2024-03-07 21:38:29 +0900

    Title: 그 다음 알파벳, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/그 다음 알파벳/next-alphabet.java
+++ 240307/그 다음 알파벳/next-alphabet.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char a = sc.next().charAt(0);
-        int b = (int)a + 1;
 
-        if(a == 'z')
+        char x = sc.next().charAt(0);
+
+        if(x == 'z')
             System.out.print("a");
-        System.out.print((char)b);
+        else
+            System.out.print((char)(x + 1));
     }
 }
