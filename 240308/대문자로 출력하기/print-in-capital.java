b452edc5d45402ee3aa7dd3d07932ca119d79b7e
sunileee <152301718+sunileee@users.noreply.github.com>
2024-03-08 09:56:59 +0900

    Title: 대문자로 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/대문자로 출력하기/print-in-capital.java
+++ 240308/대문자로 출력하기/print-in-capital.java
@@ -4,14 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.next();
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-           if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+        String str;
+        
+        str = sc.next();
+        
+        int len = str.length();
+        
+        for(int i = 0; i < len; i++) {
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
                 System.out.print(str.charAt(i));
             else if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
                 System.out.print((char)(str.charAt(i) - 'a' + 'A'));
         }
-
     }
 }
