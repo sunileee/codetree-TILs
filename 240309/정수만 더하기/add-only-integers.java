5ac6826a44a42c6430be0da9e5acceb64793a9d0
sunileee <152301718+sunileee@users.noreply.github.com>
2024-03-09 23:35:18 +0900

    Title: 정수만 더하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240309/정수만 더하기/add-only-integers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int sum = 0;
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            if ('0' <= arr[i] && arr[i] <= '9') {
+                sum += (int) arr[i];
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
